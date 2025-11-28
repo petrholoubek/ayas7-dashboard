b07e737d56b4397f1eb4be9783ef8ab4b89cd755
petrholoubek <sca.sca@centrum.cz>
2025-11-29 00:03:08 +0100

    Update menu-load.js

--- menu-load.js
+++ menu-load.js
@@ -1,15 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-   const menuContainer = document.getElementById("menu");
-
-   if (menuContainer) {
-       fetch("assets/html/menu.html")
-           .then(response => response.text())
-           .then(data => {
-               menuContainer.innerHTML = data;
-           })
-           .catch(err => {
-               console.error("Menu failed to load:", err);
-               menuContainer.innerHTML = "<p style='color:red;'>Menu load error</p>";
-           });
-   }
-});
+// menu-load.js
+fetch("menu.html")
+ .then(response => response.text())
+ .then(data => {
+   document.getElementById("main-menu").innerHTML = data;
+ })
+ .catch(err => console.error("Menu load error:", err));
